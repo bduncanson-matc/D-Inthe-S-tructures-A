9d8b184bf5ff0c0c5c30e5d4913389b9f7aecde3
Bryan Duncanson <86499328+bduncanson-matc@users.noreply.github.com>
2022-05-20 14:19:06 -0500

    Create DInTheSA.h
    Adding a class that needs work yet.

---
+++ DInTheSA.h
@@ -0,0 +1,10 @@
+//class called on by the main function 
+//purpose of this application is to create a stand alone Data Structure 
+// Alogrithm application that allows the user to learn inside of any IDE
+class DInTheSA
+{
+private:
+    Solution problem(std::string);
+    
+    
+};
