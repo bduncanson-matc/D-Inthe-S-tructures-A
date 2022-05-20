01f7d0811eb88de5df3669fcfd602afb9665ab64
Bryan Duncanson <86499328+bduncanson-matc@users.noreply.github.com>
2022-05-20 14:27:47 -0500

    Create alogNames.h
    Creates an problem names vector of strings that lists the problem names. Plan on using categories in later additions.

---
+++ alogNames.h
@@ -0,0 +1,18 @@
+//class the contains all of the data about a specific problem
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+class alog{
+private:
+    std::string textFileLocation = "./data/alogNames.txt"; //location of the text file
+    std::vector<strings> m_ProblemNames;  //vector with of all of the problem titles
+    bool haveNames;
+}
+
+public:
+
+  void createSSofNames() //creates a vector with each element containing a problems name;
+
+};
